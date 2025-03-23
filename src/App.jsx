feff5b05db8a1998c4a33ebf2a1f1d9e1b57dfeb
Surya Kashyap <ranjitha.ck15@gmail.com>
2025-03-23 15:45:25 +0530

    Initial commit - Let’s Re-Learn Again

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HelloWorld from './HelloWorld'; 
+function App() {
+  return (
+    <div>
+      <HelloWorld name="Potato!" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './HelloWorld.css';
+
+function HelloWorld(props) {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  const handleReset = () => setCount(0);
+
+  return (
+    <div className="center-box">
+      <h1>Hello, {props.name}!</h1>
+      <p>You clicked {count} times.</p>
+      <button onClick={handleIncrease}>Increase</button>{' '}
+      <button onClick={handleDecrease}>Decrease</button>{' '}
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
+
+export default HelloWorld;
